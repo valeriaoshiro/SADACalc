ed79a9bac742a9eff5e0ddfc4bc8c0d5509c0a15
Valeria Oshiro <valeriaoshiro19@gmail.com>
2019-01-15 18:44:51 -0800

    Add clear event listener

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 $(function () {
 	const operation = [];
 
-	$(".num").click(function () {
+	$(".num").click(function() {
 		let input = $(this).html();
+		let lastEl = operation[operation.length - 1];
 
-		//if last click was oper
-		if (typeof operation[operation.length - 1] === "string" || operation.length === 0) {
+		//if last click was oper or if array is empty
+		if (typeof lastEl === "string" || operation.length === 0) {
 			operation.push(parseInt(input));
 		}
 		//else if last click was number
-		else if (typeof operation[operation.length - 1] === "number") {
-			let temp = operation[operation.length - 1].toString() + input;
+		else if (typeof lastEl === "number") {
+			let temp = lastEl.toString() + input;
 			operation[operation.length - 1] = parseInt(temp);
 		}
 		console.log(operation);
 	});
 
-	$(".oper").click(function () {
+	$(".oper").click(function() {
 		let input = $(this).html();
+		let lastEl = operation[operation.length - 1];
+		
 
 		//if operation array is empty
 		if (operation.length === 0) {
 			operation.push(0);
 			operation.push(input);
 		}
-		//if last click was oper
-		else if (typeof operation[operation.length - 1] === "string") {
+		//else if last click was oper
+		else if (typeof lastEl === "string") {
 			operation[operation.length - 1] = input;
 		}
 		//else if last click was number
-		else if (typeof operation[operation.length - 1] === "number"){
+		else if (typeof lastEl === "number"){
 			operation.push(input);
 		}
 		
 		console.log(operation);
 	});
 
-	$(".enter").click(function () {
+	$(".enter").click(function() {
+		let input = $(this).html();
+		let lastEl = operation[operation.length - 1];		
+		
+		if(operation.length >= 2){
+			//if last click was operator
+			if(typeof lastEl === "string"){
+				operation.push(operation[operation.length - 2]);
+			}
 
+			//ajax
+		}
+		console.log(operation);
+	});
+
+	$(".clear").click(function() {
+		operation.length = 0;
+		console.log(operation);
 	});
 });
